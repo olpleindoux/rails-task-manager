9d7a3412c88e72060d901e00aa70c65cfed5f051
Olivier Pleindoux <pleindoux.olivier@gmail.com>
2020-05-26 18:12:36 +0200

    add task creation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,22 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    # no need for app/views/tasks/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
