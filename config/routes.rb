3c7b4576c2918e7041dfac7a5d2b9bee36a1bc3d
Olivier Pleindoux <pleindoux.olivier@gmail.com>
2020-07-08 13:37:12 +0200

    fix devise route issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
   get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -10,7 +11,6 @@ end
 
 # Rails.application.routes.draw do
 
-  devise_for :users
   # get 'pages/home'
 #   get "tasks", to: "tasks#index", as: :tasks
 
